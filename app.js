8a6c407a37936fe2dcf5fabbd92000fc5d19e159
Samuel Rincon” <samuelrincon8@yahoo.com>
2024-11-22 19:04:12 -0600

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ var logger = require('morgan');
 // Corrected paths for routes
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+var handlebars = require('hbs');
 
 var app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -22,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
