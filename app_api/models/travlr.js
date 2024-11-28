742b52ff10d074713e401c8e5715f7f1440c1b2f
Samuel Rincon” <samuelrincon8@yahoo.com>
2024-11-27 19:48:33 -0600

    Module 5 completed baseline

--- app_api/models/travlr.js
+++ app_api/models/travlr.js
@@ -11,4 +11,4 @@ const tripSchema = new mongoose.Schema({
     image: { type: String, required: true },
     description: { type: String, required: true }
 });
-module.exports = mongoose.model('Trip', tripSchema);
+module.exports = mongoose.model('trips', tripSchema);
